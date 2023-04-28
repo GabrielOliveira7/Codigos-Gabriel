f98ee573811615a3437f68ac68e5f76d212bc1c5
Gabriel <gabrieleduardobiel@gmail.com>
2023-04-28 20:02:41 -0300

    notes

--- exercicio 1.py
+++ exercicio 1.py
@@ -1,10 +1,36 @@
-#Exercício 1
-print(14+4+18+5+24+90);
+valor = input("Digite um número: ")
+numA = int(valor)
 
-#Exercício 2
-#produto a tem valor R$42
-#Desconto de 25%
+valor = input("Digite um número: ")
+numB = int(valor)
 
-input("valor do desconto");
+valor = input("Digite um número: ")
+numC = int(valor)
 
-print())
+valor = input("Digite um número: ")
+numD = int(valor)
+
+valor = input("Digite um número: ")
+numE = int(valor)
+
+
+soma = numA + numB + numC + numD + numE
+
+print(soma)
+
+
+#Análise:
+#O "input" coleta informação através do teclado
+#A informação coletada é do tipo String
+#O operador atribui a informação a variável valor do tipo String
+
+
+#Na segunda linha convertemos o conteúdo da variável valor para um número inteiro
+
+#Na penúltima linha (17) somamos todos os valores inteiros e atribuímos na variável soma
+
+#Na última linha (19) imprimimos o resultado da operação na tela do úsuario
+
+#ENTRADA E SAÍDA:
+#Input - sempre retorna um texto (String), mesmo quando a pessoa digita um número
+#Output 
