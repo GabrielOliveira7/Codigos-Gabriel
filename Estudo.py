4a2453dcd12b5e26b1f66b713003689876ead4be
Gabriel <gabrieleduardobiel@gmail.com>
2023-04-28 17:23:09 -0300

    New notes

---
+++ Estudo.py
@@ -0,0 +1,23 @@
+
+#Entrada de dados = informações fornecidas pelo usuário para o programa.
+# Processamento de dados = Operações efetuadas para obter a resposta para o usuário.
+# Saída = Resposta fornecida pelo programa para o usuário.
+
+
+
+#Operadores aritméticos:
+
+|---------------------------------------|
+|        Operador       |    Python     |
+|---------------------------------------|
+|          Soma         |       +       |
+|        Subtração      |       -       |
+|      Multiplicação    |       *       |
+|      Divisão real     |       /       |
+|        Potência       |       **      |
+|---------------------------------------|
+|      Resto divisão    |       %       |
+|     Divisão inteira   |       //      |
+|---------------------------------------|
+
+
