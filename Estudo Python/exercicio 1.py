6a7226c334f2392e89ac8ff4b4b3466d438680f5
Gabriel <gabrieleduardobiel@gmail.com>
2023-04-28 21:04:25 -0300

    a

---
+++ Estudo Python/exercicio 1.py
@@ -0,0 +1,50 @@
+valor = input("Digite um número: ")
+numA = int(valor)
+
+valor = input("Digite um número: ")
+numB = int(valor)
+
+valor = input("Digite um número: ")
+numC = int(valor)
+
+valor = input("Digite um número: ")
+numD = int(valor)
+
+valor = input("Digite um número: ")
+numE = int(valor)
+
+
+soma = numA + numB + numC + numD + numE
+
+print(soma)
+
+
+#Análise:
+#O "input" coleta informação através do teclado
+#A informação coletada é do tipo String
+#O operador atribui a informação a variável valor do tipo String
+
+
+#Na segunda linha convertemos o conteúdo da variável valor para um número inteiro
+
+#Na penúltima linha (17) somamos todos os valores inteiros e atribuímos na variável soma
+
+#Na última linha (19) imprimimos o resultado da operação na tela do úsuario
+
+#ENTRADA E SAÍDA:
+#Input - sempre retorna um texto (String), mesmo quando a pessoa digita um número
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+│   Tipo    │   Python                      │
+│───────────│───────────────────────────────│
+│  #Output  │   print(...)                  │
+│  #input   │   variavel = input("texto")   │
+│━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━│   
+
+#Conversores
+
+#int(texto) : converte o (texto) em um número inteiro se possível.
+#int(numero) : converte o (numero) em um número inteiro.
+#float(texto) : converte o (texto) em número real se possível.
+#float(número) : Converte o (numero) em um número real
+#str(número) : Converte o (número) em um texto (String)
